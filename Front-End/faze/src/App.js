e86ba4b3910009ee321496f2ce21120fbfe303dc
Dananjaya Sandakalum <h.g.d.sandakalum@gmail.com>
2021-05-15 04:46:15 +0530

    Navbar-Update

--- Front-End/faze/src/App.js
+++ Front-End/faze/src/App.js
@@ -5,23 +5,39 @@ import { Products, Navbar } from './components';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
+    // get all products
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
     
         setProducts(data);
-    };
+    }
+
+    // get cart items
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+    } 
+
+    // add items to cart
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
 
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
 
-    console.log(products);
+    console.log(cart);
 
     return (
         <div>
-            <Navbar />
-            <Products products={products} />
+            {/* change cart item quantity */}
+            <Navbar totalItems={cart.total_items} /> 
+            <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     )
 }
